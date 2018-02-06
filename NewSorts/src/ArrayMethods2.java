66c0365972364959c0bbbcecc7ff8018c39ce390
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-02-06 09:14:12 -0500

    jj sifh

--- NewSorts/src/ArrayMethods2.java
+++ NewSorts/src/ArrayMethods2.java
@@ -1,10 +1,26 @@
 
-public class ArrayMethods2
+public class ArrayMethods2//Donovan Guo
 {
-	public static String[]merge(String[]list1,String[]list2)
+	public static String[]merge(String[]list1,String[]list2) //Pre-condition both lists are sorted in alphabetical order already.
 	{
-		
-		
+	
+		String[]Combined = new String[list1.length + list2.length];//runs the code for the entire duration of the new string 
+		for(int i = 0; i < Combined.length; i++)//runs the code for the entire duration of the new string
+		{
+			int x = 0;//index for list1 
+			int y = 0; //index for list2
+			if(list1[x].compareTo(list2[y]) > 0)
+				{
+				Combined[i]= list2[y];//sets index i of "Combined" to the value of list2[y]
+				y++;// move an index forward 
+				}
+			if(list1[x].compareTo(list2[y]) < 0)
+			{
+				Combined[i]= list1[x];//vice versa of the above
+				x++;// moves list1 an index forward
+			}
+		}
+		return Combined;
 	}
 
 }
