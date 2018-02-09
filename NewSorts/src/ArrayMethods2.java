3acfeefcec617b46589429568678af1c56ea9720
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-02-09 08:30:14 -0500

    jjfish

--- NewSorts/src/ArrayMethods2.java
+++ NewSorts/src/ArrayMethods2.java
@@ -3,25 +3,26 @@ public class ArrayMethods2//Donovan Guo
 {
 	public static String[] merge(String[]list1,String[]list2) //Pre-condition both lists are sorted in alphabetical order already.
 	{
-	
+		int x = 0;//index for list1 
+		int y = 0; //index for list2
 		String[]Combined = new String[list1.length + list2.length];//runs the code for the entire duration of the new string 
 		for(int i = 0; i < Combined.length; i++)//runs the code for the entire duration of the new string
 		{
-			int x = 0;//index for list1 
-			int y = 0; //index for list2
+			
 			if ((y < list2.length) && (x < list1.length))// prevents out of index comparison
 			{
-				if(list1[x].compareTo(list2[y]) >= 0)
+				if(list1[x].compareTo(list2[y]) > 0)
 					{
 					Combined[i]= list2[y];//sets index i of "Combined" to the value of list2[y]
 					y++;// move an index forward 
 					}
-				if(list1[x].compareTo(list2[y]) < 0)
+				else if(list1[x].compareTo(list2[y]) <= 0)
 				{
 					Combined[i]= list1[x];//vice versa of the above
 					x++;// moves list1 an index forward
 				}
-				else // adds the rest of the other unfinished array to the end of the combination array 
+			}
+			else // adds the rest of the other unfinished array to the end of the combination array 
 				{
 					while(x < list1.length) // Incase list2 has been finished 
 					{
@@ -38,12 +39,18 @@ public class ArrayMethods2//Donovan Guo
 						
 				}
 			}
-		}
+		
 		return Combined;
 	}
-	//public static int partition(int[]list)
-	//{
-		//return 0;
-	//}
+	
+	public static int partition(int[]list)
+	{
+		int pivotdex = 0;
+		int checkdex = 0;
+		while(checkdex != pivotdex)
+		{
+			
+		}
+	}
 
 }
--- NewSorts/src/TestingTesting123.java
+++ NewSorts/src/TestingTesting123.java
@@ -1,17 +1,17 @@
 
 public class TestingTesting123 extends ArrayMethods2 
 {
-	public static void main(String[] args)
+	public static void main(String[] args)// Strings Must be compromised of lowercase words 
 	{
 		String[]list1 = {"mama","soe","wee","zoo"};
-		String[]list2 = {"huh","kek","lol","lul","TopKek"};
+		String[]list2 = {"huh","kek","lol","lul","topkek"};
 		print(ArrayMethods2.merge(list1, list2));
 	}
 	public static void print(String[]pop)
 	{
-	for(int i = 0; i < pop.length; i++)
-	{
-		System.out.println(pop[i]);
-	}
+		for(int i = 0; i < pop.length; i++)
+		{
+			System.out.print(" " + pop[i]);
+		}
 	}
 }
